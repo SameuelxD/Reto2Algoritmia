e480db614fe991e78fc6c44be9086a64425a7946
SameuelxD <lineaddr2004@gmail.com>
2023-12-04 16:05:01 -0500

    feat: :zap: FINISHED

---
+++ MultiplicacionRusa/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese el valor del multiplicador");
+        var multiplicador=int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese el valor del multiplicando");
+        var multiplicando=int.Parse(Console.ReadLine());
+        var resultado=0;
+        while(multiplicador>=1){
+            if(!(multiplicador%2==0)){
+                resultado=resultado+multiplicando;
+            }
+            multiplicando=multiplicando*2;
+            multiplicador=multiplicador/2;
+        }
+        Console.WriteLine($"Resultado: {resultado}");
+    }
+}
